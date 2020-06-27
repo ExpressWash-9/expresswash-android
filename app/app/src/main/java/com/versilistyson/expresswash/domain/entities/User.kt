db45d4efb2837ee7516f65ed5efb650552e61a61
Versilis Tyson <versilistyson@gmail.com>
2020-06-26 21:42:34 -0500

    - Extend user in both Client and Washer entity

--- app/app/src/main/java/com/versilistyson/expresswash/domain/entities/User.kt
+++ app/app/src/main/java/com/versilistyson/expresswash/domain/entities/User.kt
@@ -1,6 +1,47 @@
 package com.versilistyson.expresswash.domain.entities
 
-sealed class User(): Entity() {
-    data class Client(val nothing: Nothing)
-    data class Washer(val nothing: Nothing)
+sealed class User() : Entity() {
+    abstract val userId: Int
+    abstract val email: String
+    abstract val firstName: String
+    abstract val lastName: String
+    abstract val phoneNumber: String
+    abstract val city: String
+    abstract val zip: String
+    abstract val profilePicture: String
+    abstract val bannerImage: String
 }
+
+data class Client(
+    override val userId: Int,
+    override val email: String,
+    override val firstName: String,
+    override val lastName: String,
+    override val phoneNumber: String,
+    override val city: String,
+    override val zip: String,
+    override val profilePicture: String,
+    override val bannerImage: String
+
+) : User()
+data class Washer(
+    override val userId: Int,
+    val washerId: Int,
+    override val email: String,
+    override val firstName: String,
+    override val lastName: String,
+    override val phoneNumber: String,
+    override val city: String,
+    override val zip: String,
+    override val profilePicture: String,
+    override val bannerImage: String,
+    val workStatus: Boolean,
+    val rateSmall: Float,
+    val rateMedium: Float,
+    val RateLarge: Float,
+    val aboutMe: String,
+    val currentLatitude: Nothing = TODO("Change to lat after implementing MapBox"),
+    val currentLongitude: Nothing = TODO("Change to lon after implementing MapBox"),
+    val averageRating: Float,
+    val ratingCount: Int
+) : User()
